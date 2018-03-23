b77c87fd26b5f069bea310b239143c0e3b77c3e3
Guest User <Guest@epicodus-118.local>
2018-03-23 11:07:41 -0700

    add html and css structures

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 // back-end logic //
-
-
+var frontorback = $("form#front-or-back").val();
+var webordesk = $("form#web-or-not").val();
+var largeorsmall = $("form#large-or-small").val();
+var bizorgov = $("form#biz-or-gov").val();
+var extorint = $("form#ext-or-int").val();
 
 
 // front-end logic //
 $(document).ready(function() {
+  $('#front-or-back').click(function(event) {
+    event.preventDefault();
 
-  event.preventDefault();
-
-  
+  });
 });
